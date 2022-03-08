dd25a64e87479a108dcfed9faf04a7c27795ed9a
eehs2000 <35942942+eehs2000@users.noreply.github.com>
2022-03-08 20:39:41 +0900

    [feat] 더보기 menu added

--- src/pages/MainPlus.jsx
+++ src/pages/MainPlus.jsx
@@ -1,5 +1,38 @@
+import ToastGridForm from '../components/form/ToastGridForm';
+import styled from 'styled-components';
 const MainPlus = () => {
-  return <div></div>;
+  return (
+    <ListDiv>
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+      <ToastGridForm />
+    </ListDiv>
+  );
 };
+
+const ListDiv = styled.div`
+  padding-top: 5%;
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-content: center;
+`;
 
 export default MainPlus;

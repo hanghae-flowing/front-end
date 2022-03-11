70d343ecd961f0cb889b81b8e9fc6375f47554b5
Lee JuHyeon <jhmael013@gmail.com>
2022-03-11 16:31:23 +0900

    [chore] Redirect url update

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 
 //https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code
 function Login() {
-  const REDIRECT_URI = 'http://localhost:3000/member/kakao/callback';
+  const REDIRECT_URI =
+    'http://hanghae-toaster.s3-website.ap-northeast-2.amazonaws.com/member/kakao/callback';
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const onLogin = e => {};

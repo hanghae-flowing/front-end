e372f7797848c723c603ac15becd4ff152669e6d
eehs2000 <35942942+eehs2000@users.noreply.github.com>
2022-04-01 13:46:03 +0900

    [fix] bug fixed

--- src/components/modules/Square.jsx
+++ src/components/modules/Square.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { useEffect } from 'react';
 import {
   editNode,
   deleteAction,

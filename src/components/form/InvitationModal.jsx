e2eb98096ec17bb9288479f5ab0810ff3185d910
eehs2000 <35942942+eehs2000@users.noreply.github.com>
2022-04-04 19:00:30 +0900

    [feat] bookmark feature created

---
+++ src/components/form/InvitationModal.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const InvitationModal = () => {
+  return;
+};
+
+const Modal = styled.div`
+  width: 480px;
+  height: 340px;
+`;
+
+export default InvitationModal;

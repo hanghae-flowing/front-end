70149ca09bf540c66a557a103160445ceae481fe
Lee JuHyeon <jhmael013@gmail.com>
2022-04-05 03:42:07 +0900

    [feat] workspace default page featured, node ux improvement

--- src/components/tools/MileStone.jsx
+++ src/components/tools/MileStone.jsx
@@ -25,17 +25,42 @@ const MileStone = () => {
       onClick={() => {
         postMileStone.mutate(sendingData);
       }}
-    ></StyledDiv>
+    >
+      <Text>마일스톤</Text>
+    </StyledDiv>
   );
 };
 
+const Text = styled.p`
+  width: 100px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  color: #878787;
+  display: none;
+  position: absolute;
+  top: -24px;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
 const StyledDiv = styled.div`
+  position: relative;
   width: 100px;
   height: 38px;
   border-radius: 40px;
   background-color: #eee0ff;
   box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.15);
   cursor: pointer;
+  transition: all 0.1s ease-in-out;
+  margin: 0 10px;
+  &:hover {
+    transform: scale(1.3) translateY(-10px);
+    margin: 0 20px;
+    ${Text} {
+      display: block;
+    }
+  }
 `;
 
 export default MileStone;
